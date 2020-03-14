32417cf0a023f44e93810b5479b345918d9019ac
Andres Martin <monkeyandres@protonmail.com>
2020-03-14 13:14:50 +0100

    Add group chats support and multiline messages

--- src/app/Chat.jsx
+++ src/app/Chat.jsx
@@ -13,17 +13,22 @@ const Chat = ({ chat, selectedContact, scrollerRef }) => {
     <>
       <h1 className="chat-header">Chat Content</h1>
       <div className="chat-grid">
-        {chat.messages.map((msg, i) => (
-          <div
-            key={i}
-            className={`message ${
-              selectedContact === msg.sender ? 'mine' : 'others'
-            }`}
-          >
-            <span className="content">{msg.message}</span>
-            <span className="date">{dateToLocateTimeString(msg.date)}</span>
-          </div>
-        ))}
+        {chat.messages.map((msg, i) => {
+          const isMine = selectedContact === msg.sender
+
+          return isMine ? (
+            <div key={i} className="message mine">
+              <pre className="content">{msg.message}</pre>
+              <span className="date">{dateToLocateTimeString(msg.date)}</span>
+            </div>
+          ) : (
+            <div key={i} className="message others">
+              {isGroup ? <p className="sender">{msg.sender}</p> : null}
+              <pre className="content">{msg.message}</pre>
+              <span className="date">{dateToLocateTimeString(msg.date)}</span>
+            </div>
+          )
+        })}
       </div>
     </>
   )
