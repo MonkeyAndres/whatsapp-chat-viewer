40f8931b072a3dd50f64fd5a143ca8a5ac971231
Andres Martin <monkeyandres@protonmail.com>
2020-03-15 18:38:03 +0100

    Add space between rows

--- src/ui/main/ChatMessage.jsx
+++ src/ui/main/ChatMessage.jsx
@@ -5,10 +5,13 @@ const ChatMessage = ({ style, msg, isMine, isGroup }) => {
   const messageClassName = isMine ? 'mine' : 'others'
 
   return (
-    <div className={`message ${messageClassName}`} style={style}>
-      {isGroup && !isMine ? <p className="sender">{msg.sender}</p> : null}
-      <pre className="content">{msg.message}</pre>
-      <span className="date">{dateToLocateTimeString(msg.date)}</span>
+    // HACK: Needed in order to have padding between rows
+    <div className={`message-container-${messageClassName}`} style={style}>
+      <div className={`message ${messageClassName}`}>
+        {isGroup && !isMine ? <p className="sender">{msg.sender}</p> : null}
+        <pre className="content">{msg.message}</pre>
+        <span className="date">{dateToLocateTimeString(msg.date)}</span>
+      </div>
     </div>
   )
 }
