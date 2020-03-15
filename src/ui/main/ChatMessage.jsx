88f715a2f090f4dce9fc4828fcfccf22964f6398
Andres Martin <monkeyandres@protonmail.com>
2020-03-15 18:55:35 +0100

    Fix messages always with same width

--- src/ui/main/ChatMessage.jsx
+++ src/ui/main/ChatMessage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import * as R from 'ramda'
 import { dateToLocateTimeString } from '../../app/utils'
 
 const ChatMessage = ({ style, msg, isMine, isGroup }) => {
   const messageClassName = isMine ? 'mine' : 'others'
 
+  const filteredStyles = R.omit(['width', 'left'], style)
+
   return (
     // HACK: Needed in order to have padding between rows
-    <div className={`message-container-${messageClassName}`} style={style}>
+    <div
+      style={filteredStyles}
+      className={`message-container-${messageClassName}`}
+    >
       <div className={`message ${messageClassName}`}>
         {isGroup && !isMine ? <p className="sender">{msg.sender}</p> : null}
         <pre className="content">{msg.message}</pre>
