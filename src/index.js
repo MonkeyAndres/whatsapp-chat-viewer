40774dc5708593975d476b29bdccaa37fd2853fb
Andres Martin <monkeyandres@protonmail.com>
2020-03-20 20:07:07 +0100

    Remove react-virtualized css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.scss'
-import 'react-virtualized/styles.css'
 import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 
