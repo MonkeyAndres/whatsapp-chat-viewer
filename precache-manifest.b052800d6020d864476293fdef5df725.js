1c2594be6eeabaa51a701654bf5ea89bb881d951
Andres Martin <monkeyandres@protonmail.com>
2020-03-21 14:25:23 +0100

    Updates

---
+++ precache-manifest.b052800d6020d864476293fdef5df725.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beaecb299f376d9b05fae61a46ade29",
+    "url": "/whatsapp-chat-viewer/index.html"
+  },
+  {
+    "revision": "c9d843793f7b8463e943",
+    "url": "/whatsapp-chat-viewer/static/css/main.e7215694.chunk.css"
+  },
+  {
+    "revision": "2609b47a5a5ca08304ce",
+    "url": "/whatsapp-chat-viewer/static/js/2.4b08f1cf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/whatsapp-chat-viewer/static/js/2.4b08f1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d843793f7b8463e943",
+    "url": "/whatsapp-chat-viewer/static/js/main.d5da0b46.chunk.js"
+  },
+  {
+    "revision": "818d5b8df367051029b5",
+    "url": "/whatsapp-chat-viewer/static/js/runtime-main.fa9495f7.js"
+  },
+  {
+    "revision": "3b5d5b599495d94e1882520373ecf57b",
+    "url": "/whatsapp-chat-viewer/static/media/arrow-left.3b5d5b59.svg"
+  },
+  {
+    "revision": "749f1ba7693d0f6280b0078511cc99ae",
+    "url": "/whatsapp-chat-viewer/static/media/upload.749f1ba7.svg"
+  }
+]);
