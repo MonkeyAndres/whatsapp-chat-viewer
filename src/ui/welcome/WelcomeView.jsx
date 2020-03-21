383ae49e7f480cac4b800f56c01b03521a3c7728
Andres Martin <monkeyandres@protonmail.com>
2020-03-21 18:23:34 +0100

    Add quick documentation to project

--- src/ui/welcome/WelcomeView.jsx
+++ src/ui/welcome/WelcomeView.jsx
@@ -13,10 +13,12 @@ const WelcomeView = ({ onSelectFile }) => {
     <div className="card welcome">
       <h1 className="welcome-title">WhatsApp Chat Viewer</h1>
       <div className="paragraph-1">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum esse vel
-        necessitatibus! Suscipit a aliquid ea natus dolores temporibus deserunt
-        quod mollitia voluptates modi earum, odio eius explicabo asperiores
-        adipisci?
+        <p>
+          WhatsApp chat viewer is an online tool that allows you to view your
+          WhatsApp exported chats. The web doesn't store any user data it only
+          formats the chat messages in a more visual way in order to help users
+          to read exported chats.
+        </p>
       </div>
 
       {isAdvancedUpload ? (
@@ -27,17 +29,35 @@ const WelcomeView = ({ onSelectFile }) => {
 
       <div className="paragraph-2">
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          repellat corporis incidunt itaque similique, nam quod illo esse amet
-          sequi laboriosam est, dicta assumenda aliquid, inventore animi
-          asperiores velit non?
+          In order to be able to use this tool, you will need an exported
+          WhatsApp chat. This file has the extension txt. Bellow, I will show
+          you how you can export a WhatsApp chat.
+        </p>
+
+        <h2>How to export a WhatsApp Chat?</h2>
+        <p>Follow the "Export chat history" part of the tutorials bellow.</p>
+        <p>
+          <a href="https://faq.whatsapp.com/en/android/23756533/">
+            For Android devices
+          </a>
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-          repellat corporis incidunt itaque similique, nam quod illo esse amet
-          sequi laboriosam est, dicta assumenda aliquid, inventore animi
-          asperiores velit non?
+          <a href="https://faq.whatsapp.com/en/iphone/26000285/?category=5245251">
+            For iPhone devices
+          </a>
         </p>
+
+        <h2>About this project.</h2>
+        <p>
+          This is a personal project developed by{' '}
+          <a href="https://github.com/MonkeyAndres">@MonkeyAndres</a>. Is
+          completely free and open source. You can access the source code{' '}
+          <a href="https://github.com/MonkeyAndres/whatsapp-chat-viewer">
+            here.
+          </a>
+        </p>
+
+        <br />
       </div>
     </div>
   )
