4a9cdaaa387db62557d6785e54017d0466a9420e
Andres Martin Angulo <monkeyandres@protonmail.com>
2023-09-29 19:47:10 +0200

    fix(parser): add a more flexible whatsapp message regexp

--- src/lib/whatsapp-parser/constants.js
+++ src/lib/whatsapp-parser/constants.js
@@ -3,4 +3,4 @@ export const TIME_SEPARATOR = ':'
 
 export const SECURITY_MESSAGE_REGEXP = /are now secured with end-to-end encryption./
 
-export const WHATSAPP_MESSAGE_REGEXP = /\[(\d{2}\/\d{2}\/\d{4}), (\d{2}:\d{2}:\d{2})\] ([^:]+): (([^[\]])*)/i
+export const WHATSAPP_MESSAGE_REGEXP = /\[(\d{1,2}\/\d{1,2}\/\d{2,4}), (\d{2}:\d{2}:\d{2})\] ([^:]+): (([^[\]])*)/i
